75c082b7cadc51872717306346bc88ff95c0502f
Howard <howard@HowarddeMacBook-Pro.local>
2026-03-06 11:34:50 +0800

    chore: setup github pages basePath

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: process.env.NODE_ENV === 'production' ? "/ai-stock-scanner" : "",
+  assetPrefix: process.env.NODE_ENV === 'production' ? "/ai-stock-scanner/" : "",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
